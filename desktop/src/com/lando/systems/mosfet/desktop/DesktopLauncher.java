8ea789c31955e0dc19881ab5e8627546a68536b9
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2015-09-10 21:46:32 -0500

    Initial commit

---
+++ desktop/src/com/lando/systems/mosfet/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.lando.systems.mosfet.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.lando.systems.mosfet.MosfetGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MosfetGame(), config);
+	}
+}
