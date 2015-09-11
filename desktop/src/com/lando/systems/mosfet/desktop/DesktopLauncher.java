827754b62917722612a02a520f82ffeff72494ac
Doug Graham <dsgraham@gmail.com>
2015-09-10 21:57:34 -0500

    Make Desktop match android aspect ratio

--- desktop/src/com/lando/systems/mosfet/desktop/DesktopLauncher.java
+++ desktop/src/com/lando/systems/mosfet/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.lando.systems.mosfet.MosfetGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 480;
+		config.height = 800;
+		config.title = "MosFet";
 		new LwjglApplication(new MosfetGame(), config);
 	}
 }
