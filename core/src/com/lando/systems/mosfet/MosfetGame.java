08888e858fdd902998fc4a37ccb497b9000e0905
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2015-09-15 20:58:12 -0500

    Start on gameplay screen on level 1

--- core/src/com/lando/systems/mosfet/MosfetGame.java
+++ core/src/com/lando/systems/mosfet/MosfetGame.java
@@ -2,15 +2,20 @@ package com.lando.systems.mosfet;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.lando.systems.mosfet.screens.LevelEditorScreen;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.utils.Json;
+import com.lando.systems.mosfet.screens.GamePlayScreen;
 import com.lando.systems.mosfet.utils.Assets;
+import com.lando.systems.mosfet.world.Level;
 
 public class MosfetGame extends Game {
 
 	@Override
 	public void create () {
 		Assets.load();
-		setScreen(new LevelEditorScreen(this));
+
+		final FileHandle levelFile = Gdx.files.internal("levels/level1.lvl");
+		setScreen(new GamePlayScreen(this, (new Json()).fromJson(Level.class, levelFile)));
 	}
 
 	@Override
